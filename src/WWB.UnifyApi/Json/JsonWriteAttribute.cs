e658ac1329a11d3eaebca591c803955ea22c2cfc
王爷 <22379099@qq.com>
2023-10-14 22:45:08 +0800

    增加OperateFilter抽象类

---
+++ src/WWB.UnifyApi/Json/JsonWriteAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WWB.UnifyApi.Json
+{
+    public class JsonAttribute : Attribute
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// 属性是否可以被序列化
+        /// </summary>
+        public bool Readable { get; set; } 
+        
+        /// <summary>
+        /// 属性是否可以被反序列化
+        /// </summary>
+        public bool Writable { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public object? DefaultValue { get; set; }
+    
+        public JsonAttribute()
+        {
+            this.Readable = true;
+            this.Writable = true;
+        }
+    }
+
+}
